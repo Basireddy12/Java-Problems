b39d052dcc5a383bea6993ac2510da5555308f51
Basireddy12 <bjahnavireddy7@gmail.com>
2025-06-13 15:35:40 +0530

    Add files via upload

---
+++ datatypes.java
@@ -0,0 +1,26 @@
+public class datatypes{
+    public static void main(String[] args) {
+        byte a = 127;
+        short b = 1239;
+        int c = 2_45_343;
+        long d = 3_43_343_343;
+        float e = 3.4e+038f;
+        double f = 1.8e+307;
+          char myletter ='h';
+          char value_ascii =65;
+          boolean g =true;
+          String h ="hello";
+          System.out.println("byte:" + a);
+          System.out.println("short:" + b);
+          System.out.println("int:" + c);
+          System.out.println("float:" + d);
+          System.out.println("double:" + e);
+          System.out.println("char:" + myletter);
+          System.out.println("charcter:" + value_ascii);
+          System.out.println("boolean:" + g);
+          System.out.println("String:" + h);
+
+
+
+    }
+}
