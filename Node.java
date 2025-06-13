3e24c84675398d7b44fcd671583254b9454d17f3
Basireddy12 <bjahnavireddy7@gmail.com>
2025-06-13 15:40:45 +0530

    Update Node.java
    Write a  java program  to count the number of nodes in singleLinkedList?

--- Node.java
+++ Node.java
@@ -1,4 +1,4 @@
- class Node {
+class Node {
     int data;
     Node next;
 
@@ -8,9 +8,10 @@
     }
 }
 
-class SinglyLinkedList {
+public class  SinglyLinkedList{
     Node head;
 
+    // Insert at end
     void insertAtEnd(int data) {
         Node newNode = new Node(data);
         if (head == null) {
@@ -24,21 +25,39 @@ class SinglyLinkedList {
         current.next = newNode;
     }
 
+    // Count nodes
+    int countNodes() {
+        int count = 0;
+        Node current = head;
+        while (current != null) {
+            count++;
+            current = current.next;
+        }
+        return count;
+    }
+
+    // Display nodes
     void display() {
         Node current = head;
         while (current != null) {
             System.out.print(current.data + " ");
             current = current.next;
         }
+        System.out.println();
     }
 
+    // Main method
     public static void main(String[] args) {
         SinglyLinkedList list = new SinglyLinkedList();
         list.insertAtEnd(10);
         list.insertAtEnd(20);
         list.insertAtEnd(30);
-        list.display();  // Output: 10 20 30
+        list.insertAtEnd(40);
+
+        System.out.print("Linked List: ");
+        list.display();
+
+        int count = list.countNodes();
+        System.out.println("Number of nodes: " + count);
     }
 }
- 
-
